ec2f22d4c2de0a69fae2fbd4e57650f204bbeea0
Inho Oh <webispy@gmail.com>
2022-09-15 10:52:33 +0000

    libnugu-plugins: add 1.7.3 bottle.

--- Formula/libnugu-plugins.rb
+++ Formula/libnugu-plugins.rb
@@ -9,6 +9,12 @@ class LibnuguPlugins < Formula
   # brew install --build-from-source --HEAD libnugu
   head "https://github.com/nugu-developers/nugu-linux.git", branch: "master"
 
+  bottle do
+    root_url "https://github.com/webispy/homebrew-sdk/releases/download/libnugu-plugins-1.7.3"
+    sha256 cellar: :any, monterey: "6aa29b640972ce216963f03de0441b4f55733adb949e09a750ae09cc1d71e2cf"
+    sha256 cellar: :any, big_sur:  "dba4605ade65d8440d4f87e9da7b43384f801f7966edc81fb0e9fe60a2bb0bf3"
+  end
+
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
   depends_on "gst-plugins-bad"

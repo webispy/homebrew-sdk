ea3430dc89c9ef1a4aabdf7eb8b552be46196689
Inho Oh <webispy@gmail.com>
2022-09-15 19:56:31 +0900

    libnugu-examples 1.7.3 (new formula)
    Closes #3.
    
    Signed-off-by: Inho Oh <webispy@gmail.com>

---
+++ Formula/libnugu-examples.rb
@@ -0,0 +1,31 @@
+class LibnuguExamples < Formula
+  desc "Example applications for NUGU SDK"
+  homepage "https://github.com/nugu-developers/nugu-linux"
+  url "https://github.com/webispy/nugu-linux/archive/c0fd086.tar.gz"
+  version "1.7.3"
+  sha256 "95b7fdc3f8686ff8f736ed6337ea9eaa4ddd0fe0db6cc299a9f1cc4933cf2b68"
+  license "Apache-2.0"
+
+  # brew install --build-from-source --HEAD libnugu
+  head "https://github.com/nugu-developers/nugu-linux.git", branch: "master"
+
+  depends_on "cmake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "libnugu"
+  depends_on "libnugu-plugins"
+
+  def install
+    args = %w[
+      -DENABLE_EXAMPLES_ONLY=ON
+      -DPACKAGING=ON
+    ]
+
+    system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    system "true"
+  end
+end

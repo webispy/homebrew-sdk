ff603c0bf0590469da85923bd38446e36ccf726a
Inho Oh <webispy@gmail.com>
2022-09-15 11:24:09 +0000

    libnugu-examples: add 1.7.3 bottle.

--- Formula/libnugu-examples.rb
+++ Formula/libnugu-examples.rb
@@ -9,6 +9,12 @@ class LibnuguExamples < Formula
   # brew install --build-from-source --HEAD libnugu
   head "https://github.com/nugu-developers/nugu-linux.git", branch: "master"
 
+  bottle do
+    root_url "https://github.com/webispy/homebrew-sdk/releases/download/libnugu-examples-1.7.3"
+    sha256 monterey: "1f21a5b5c3a9dfde2a51047a3dc26f8d0287b0cdeaccbcb07de02e067e493aea"
+    sha256 big_sur:  "a35431d4bcfd066b6c3ee2800783d8177dc0fd2953fe8cf4c4d4c083337774f2"
+  end
+
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
   depends_on "libnugu"

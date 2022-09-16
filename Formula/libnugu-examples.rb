a3f2ef808251e05f77495486c217831b4c31f9ea
Inho Oh <webispy@gmail.com>
2022-09-16 02:01:41 +0000

    libnugu-examples: update 1.7.3 bottle.

--- Formula/libnugu-examples.rb
+++ Formula/libnugu-examples.rb
@@ -11,8 +11,9 @@ class LibnuguExamples < Formula
 
   bottle do
     root_url "https://github.com/webispy/homebrew-sdk/releases/download/libnugu-examples-1.7.3"
-    sha256 monterey: "1f21a5b5c3a9dfde2a51047a3dc26f8d0287b0cdeaccbcb07de02e067e493aea"
-    sha256 big_sur:  "a35431d4bcfd066b6c3ee2800783d8177dc0fd2953fe8cf4c4d4c083337774f2"
+    rebuild 1
+    sha256 monterey: "c86a84300f57d17dfc76ee2802d244118b04c734d93eede65e6e6ed40feb54b2"
+    sha256 big_sur:  "b3f739a4fffda6d4cc9252e6ffb9694813ba7ac79f9bd03588fbc5885ebf4475"
   end
 
   depends_on "cmake" => :build

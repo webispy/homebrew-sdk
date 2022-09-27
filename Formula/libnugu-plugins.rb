ea4fd87f182d8a5caeb57d9f8902c3a73b000d5c
Inho Oh <inho.oh@sk.com>
2022-09-27 18:45:14 +0900

    update libnugu-plugins
    Signed-off-by: Inho Oh <inho.oh@sk.com>

--- Formula/libnugu-plugins.rb
+++ Formula/libnugu-plugins.rb
@@ -1,9 +1,9 @@
 class LibnuguPlugins < Formula
   desc "Default Plugins for NUGU SDK"
   homepage "https://github.com/nugu-developers/nugu-linux"
-  url "https://github.com/nugu-developers/nugu-linux/archive/a5bffd0.tar.gz"
+  url "https://github.com/nugu-developers/nugu-linux/archive/cac6425.tar.gz"
   version "1.7.3"
-  sha256 "1d7846706201448156b9a6ba38535dcae6b785cc96ccd102f6da40b91052065b"
+  sha256 "b19cc823732cf7ebecffc792e99d400b3173dc8b1fa1f28f7c112d5ede5b03f5"
   license "Apache-2.0"
 
   # brew install --build-from-source --HEAD libnugu
@@ -19,14 +19,15 @@ class LibnuguPlugins < Formula
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
   depends_on "gst-plugins-bad"
+  depends_on "gst-plugins-base"
   depends_on "gst-plugins-good"
   depends_on "gstreamer"
   depends_on "libnugu"
+  depends_on "libnugu-epd"
   depends_on "gst-plugins-ugly" => :recommended
 
   def install
     args = %w[
-      -DENABLE_VENDOR_LIBRARY=OFF
       -DENABLE_PLUGINS_ONLY=ON
       -DPACKAGING=ON
     ]

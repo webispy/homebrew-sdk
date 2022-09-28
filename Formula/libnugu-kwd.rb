066f68a983d194f2e2bd91fae1e702593bd0652a
Inho Oh <webispy@gmail.com>
2022-09-28 10:45:31 +0900

    rebuild libnugu-kwd
    Signed-off-by: Inho Oh <webispy@gmail.com>

--- Formula/libnugu-kwd.rb
+++ Formula/libnugu-kwd.rb
@@ -8,13 +8,6 @@ class LibnuguKwd < Formula
   sha256 "ca0c194fc6aa40fc7e4a1d1198819c2971b80d6dca4795667ba0ad689bdd27bd"
   license "Apache-2.0"
 
-  bottle do
-    root_url "https://github.com/webispy/homebrew-sdk/releases/download/libnugu-kwd-0.9.38"
-    rebuild 2
-    sha256 cellar: :any, monterey: "58340893d42bcf83fb997c7b139d47c3d1a91f28eeb3e03c83c1c822a1d6d67b"
-    sha256 cellar: :any, big_sur:  "c51972d5ed64594bc1c13e6635fee9343ed5fd7773aafb043680180608ae445a"
-  end
-
   depends_on "cmake" => :build
 
   def install

d6fac868f29c017ef422b773a68cd829708b322b
Inho Oh <webispy@gmail.com>
2022-09-28 02:08:08 +0000

    libnugu-kwd: add 0.9.38 bottle.

--- Formula/libnugu-kwd.rb
+++ Formula/libnugu-kwd.rb
@@ -8,6 +8,14 @@ class LibnuguKwd < Formula
   sha256 "ca0c194fc6aa40fc7e4a1d1198819c2971b80d6dca4795667ba0ad689bdd27bd"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://github.com/webispy/homebrew-sdk/releases/download/libnugu-kwd-0.9.38"
+    rebuild 3
+    sha256 cellar: :any, arm64_monterey: "a531b4f7168dced23960234770fbe33aae3897519e5a394c78706560e3d5f890"
+    sha256 cellar: :any, monterey:       "bd084749a53dfca65350920c019f7a2f3213a65a93a6dd838fbfe6898008e678"
+    sha256 cellar: :any, big_sur:        "c1bf6d4e23d16af0a01f1d458748da4e0cefcfff085d782858de75002329a75b"
+  end
+
   depends_on "cmake" => :build
 
   def install

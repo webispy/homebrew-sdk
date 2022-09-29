afddea3801446ed122ce49bc6e0f592d6378216a
Inho Oh <webispy@gmail.com>
2022-09-29 16:00:31 +0900

    libnugu-kwd: rebuild
    update libnugu-kwd
    
    Closes #19.
    
    Signed-off-by: Inho Oh <webispy@gmail.com>

--- Formula/libnugu-kwd.rb
+++ Formula/libnugu-kwd.rb
@@ -1,11 +1,8 @@
-require_relative "../lib/GitHubPrivateRepositoryReleaseDownloadStrategy"
-
 class LibnuguKwd < Formula
   desc "C/C++ library for NUGU SDK Keyword detector"
-  homepage "https://github.com/nugulinux/keyword-detector"
-  # Use URL fragment to prevent brew audit failures for private repository.
-  url "https://github.com/nugulinux/#keyword-detector/releases/download/0.9.38ubuntu6osx/libnugu-kwd-src.tgz", using: GitHubPrivateRepositoryReleaseDownloadStrategy
-  sha256 "ca0c194fc6aa40fc7e4a1d1198819c2971b80d6dca4795667ba0ad689bdd27bd"
+  homepage "https://github.com/nugulinux/nugu-kwd"
+  url "https://github.com/nugulinux/nugu-kwd/releases/download/0.9.38ubuntu6/libnugu-kwd-src.tgz"
+  sha256 "ad524e18328dde5f07381cfad56a62a03f11108db3696a7d1f72e7996e8257aa"
   license "Apache-2.0"
 
   bottle do

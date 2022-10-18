1ac22d3f3ed8d345efc1ff865d3bc4f8690fe8c3
Inho Oh <webispy@gmail.com>
2022-10-18 10:39:56 +0900

    nugu-dbus-daemon: rebuild
    update nugu-dbus-daemon
    
    Closes #30.
    
    Signed-off-by: Inho Oh <webispy@gmail.com>

--- Formula/nugu-dbus-daemon.rb
+++ Formula/nugu-dbus-daemon.rb
@@ -1,16 +1,10 @@
 class NuguDbusDaemon < Formula
   desc "SESSION_BUS based dbus-daemon for NUGU service"
   homepage "https://github.com/nugulinux/nugu-daemon"
-  url "https://github.com/nugulinux/nugu-daemon/archive/065c021.tar.gz"
+  url "https://github.com/nugulinux/nugu-daemon/archive/3c2c624.tar.gz"
   version "1.7.3"
-  sha256 "20d9d12521add78db62be75a93cee3299d4173e8ebf73500d47f868f9307a57b"
+  sha256 "9bd7b933a4c1ea2b906362dfd47091640740345ddbed3c5528a38ed0117ef06c"
   license "Apache-2.0"
-
-  bottle do
-    root_url "https://github.com/webispy/homebrew-sdk/releases/download/nugu-dbus-daemon-1.7.3"
-    sha256 cellar: :any_skip_relocation, monterey: "4b8afd995b044c9f8e0d4ebe5cb4c6661075a1e7bf2df949a590131113a35e88"
-    sha256 cellar: :any_skip_relocation, big_sur:  "656fc969a8ca73f051486eff48fafb3f2d9d45e11433254284cb29f135b79dba"
-  end
 
   depends_on "cmake" => :build
   depends_on "dbus"
@@ -19,6 +13,7 @@ class NuguDbusDaemon < Formula
     args = %W[
       -DHOMEBREW_PREFIX=#{HOMEBREW_PREFIX}
       -DDBUS_PATH=#{Formula["dbus"].bin}
+      -DSOCKET_PATH=#{sock_path}
     ]
 
     system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
@@ -29,11 +24,15 @@ class NuguDbusDaemon < Formula
     "com.sktnugu.nugu_dbus_daemon"
   end
 
+  def sock_path
+    "/var/run/nugu_dbus_daemon.sock"
+  end
+
   def caveats
     <<~EOS
       To use dbus session bus, add the following to your .zshrc:
 
-        export DBUS_SESSION_BUS_ADDRESS=unix:path=#{HOMEBREW_PREFIX}/var/run/nugu_dbus_daemon.sock
+        export DBUS_SESSION_BUS_ADDRESS=unix:path=#{HOMEBREW_PREFIX}#{sock_path}
     EOS
   end
 

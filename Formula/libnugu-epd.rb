93dfb501a328554c9cfceff654e1aa955bf2303e
Inho Oh <webispy@gmail.com>
2024-06-24 06:10:37 +0000

    libnugu-epd: update 2.1.11 bottle.

--- Formula/libnugu-epd.rb
+++ Formula/libnugu-epd.rb
@@ -7,10 +7,10 @@ class LibnuguEpd < Formula
 
   bottle do
     root_url "https://github.com/webispy/homebrew-sdk/releases/download/libnugu-epd-2.1.11"
-    rebuild 3
-    sha256 cellar: :any, arm64_monterey: "0bf65fc13928a4e3126804ef093571ca1a0fde86bfc87921f5c56d8a277e5b87"
-    sha256 cellar: :any, monterey:       "a077ec2836553320a5e19ffc777e94136ab9306a52602335c2ad6bf4a9ceb4b1"
-    sha256 cellar: :any, big_sur:        "f57361c2f6169d6b69f86838dc702dfb38dd0f99f900036922944c19c8db3584"
+    rebuild 4
+    sha256 cellar: :any, arm64_sonoma: "e90b34ec38078fa0bc73d698c0f0d26741d7170b8925eafea7c36ec37ceb85cd"
+    sha256 cellar: :any, ventura:      "1cad824fcc042c318ff89977f98f5cb4c3c793301265db5ca8aa75e4ea49de42"
+    sha256 cellar: :any, monterey:     "8ae053ff2b9eee062045b379d762b174a5980eac03b2acea191225cf545dd352"
   end
 
   depends_on "cmake" => :build

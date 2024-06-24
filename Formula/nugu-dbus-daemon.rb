0bf6e5373072693a93236e9d5578cee30bf45873
Inho Oh <webispy@gmail.com>
2024-06-24 09:56:50 +0000

    nugu-dbus-daemon: add 0.1.0 bottle.

--- Formula/nugu-dbus-daemon.rb
+++ Formula/nugu-dbus-daemon.rb
@@ -6,6 +6,13 @@ class NuguDbusDaemon < Formula
   sha256 "4e02dadd0b14f6075733002383e1033a909f699d9c048e2e130033757bbd21ed"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://github.com/webispy/homebrew-sdk/releases/download/nugu-dbus-daemon-0.1.0"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "dfc1a9fcdb743e882b8fa913383aa40cd518b65b10acb2affc6e5737b22e2122"
+    sha256 cellar: :any_skip_relocation, ventura:      "75112891d49d2eaea5aac800585e3af0697745ed6153bbf8081a5eaec412e51b"
+    sha256 cellar: :any_skip_relocation, monterey:     "9332b24c0e6c1715eee721c300457f02158f62adbef0090389019702450a502f"
+  end
+
   depends_on "cmake" => :build
   depends_on "dbus"
 

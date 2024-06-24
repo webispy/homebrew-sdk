eee4fb0280be23f232e039dfadc21d57667e2c81
Inho Oh <webispy@gmail.com>
2024-06-24 07:18:45 +0000

    libnugu: update 1.7.5 bottle.

--- Formula/libnugu.rb
+++ Formula/libnugu.rb
@@ -11,9 +11,10 @@ class Libnugu < Formula
 
   bottle do
     root_url "https://github.com/webispy/homebrew-sdk/releases/download/libnugu-1.7.5"
-    sha256 cellar: :any, arm64_sonoma: "aa81fcd8ee4540bf6e9ef5c69279d2d025629d12f36c18f5e80f0be592625ab8"
-    sha256 cellar: :any, ventura:      "4d1e3a8f8f76039c6371e3253197be648e08336d6a2bf5d33f1011d582e0067b"
-    sha256 cellar: :any, monterey:     "7909e78b832cd39bf2fb5f9c8a8fe543f30804daf58b12cd0e1bdc030accb5c8"
+    rebuild 1
+    sha256 cellar: :any, arm64_sonoma: "aee1a105c4161fab8cebfc96a797b2dc08bd3d5cd6c9d5ab3bc5d8a3e13e3c8e"
+    sha256 cellar: :any, ventura:      "b02cd5df3c78e3d502a1870beb2fda6483e57a23b79cda5607778927fe1be2b7"
+    sha256 cellar: :any, monterey:     "c13e6895989a595cd9dc3d6791388d112045e5e631ad348337c4757618843e32"
   end
 
   depends_on "cmake" => :build

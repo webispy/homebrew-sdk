5e02dcbd12a922b29bedf8a26e3eb91342c83191
Inho Oh <webispy@gmail.com>
2024-06-25 06:33:24 +0000

    nugu-daemon: update 1.7.5 bottle.

--- Formula/nugu-daemon.rb
+++ Formula/nugu-daemon.rb
@@ -10,9 +10,10 @@ class NuguDaemon < Formula
 
   bottle do
     root_url "https://github.com/webispy/homebrew-sdk/releases/download/nugu-daemon-1.7.5"
-    sha256 arm64_sonoma: "9c688cb5ccfd3b05c8e20de496590af4d688652468fecf6a45c87339a684bd1a"
-    sha256 ventura:      "0bcd4bdaa5626f56e22bfc7416d08be98b424d3ed90e55717cc3006076b87aa1"
-    sha256 monterey:     "41f0febb8c0ee1197046299c4bb6186f608c98783e7435e06bc8e41ee33ef120"
+    rebuild 1
+    sha256 arm64_sonoma: "afc0c955cf73527ee8a67cfc7a773822403c3a3064f7b49105e7a7be4f38d4fb"
+    sha256 ventura:      "8671181f1f121a69b1271dcdc17453fe0361b65657a8f79edf372521f442eb90"
+    sha256 monterey:     "d3de356fd039368c5403f3221cd190cee8c3b206352305102e7bd4de20acb885"
   end
 
   depends_on "cmake" => :build

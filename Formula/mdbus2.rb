cb4b0a61199e71553ae8f42d0807c6ab37d19176
Inho Oh <webispy@gmail.com>
2024-06-27 01:52:59 +0000

    mdbus2: add 2.3.3 bottle.

--- Formula/mdbus2.rb
+++ Formula/mdbus2.rb
@@ -9,6 +9,13 @@ class Mdbus2 < Formula
   # brew install --build-from-source --HEAD mdbus2
   head "https://github.com/webispy/mdbus.git", branch: "master"
 
+  bottle do
+    root_url "https://github.com/webispy/homebrew-sdk/releases/download/mdbus2-2.3.3"
+    sha256 cellar: :any, arm64_sonoma: "ec62d6c0e839f53c6d79f397c32c771d38002804e4f7da4c4ef2e47ab64e33f7"
+    sha256 cellar: :any, ventura:      "91016493fbb111ce46d15b375a00ad8f51a6f991dfea36ef19f5b3d2706a7bab"
+    sha256 cellar: :any, monterey:     "9743d0b4e6eced9e96e2e39330d74ffdd235aa5b4763b8774719bd75b4bc3edb"
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
